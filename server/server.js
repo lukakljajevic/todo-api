61730b93499af9ffc859fdadab463bd903592b9d
Luka Kljajevic <luka.kljajevic.dev@gmail.com>
2019-02-16 11:30:26 +0100

    Added get and post routes

---
+++ server/server.js
@@ -0,0 +1,29 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let {mongoose} = require('./db/mongoose');
+let {Todo} = require('./models/todo');
+let {User} = require('./models/user');
+
+let app = express();
+
+app.use(bodyParser.json());
+
+app.get('/todos', (req, res) => {
+  Todo.find({}).then(todos => res.send({todos}), e => res.status(400).send(e));
+});
+
+app.post('/todos', (req, res) => {
+  console.log(req.body);
+  let todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then(doc => {
+    res.send(doc);
+  }, err => {
+    res.status(400).send(err);
+  });
+});
+
+app.listen(3000, () => console.log('Server started on port 3000.'));
+module.exports = {app};
