0d1432650930759410242338a64db4f46e24c042
Luka Kljajevic <luka.kljajevic.dev@gmail.com>
2019-02-22 11:46:25 +0100

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -47,6 +47,15 @@ UserSchema.methods.generateAuthToken = function() {
   return this.save().then(() => token);
 };
 
+UserSchema.methods.removeToken = function(token) {
+  let user = this;
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
 UserSchema.statics.findByToken = function(token) {
   let User = this;
   let decoded;
